50f83f71756f1e8571761d9da2b257b861fa21d4
shaiedri <shaiedri850@gmail.com>
2023-05-30 16:55:24 +0300

    Added Primary Closes #6

--- src/main/java/com/blackops/ditutorial/services/PrimaryGreetingService.java
+++ src/main/java/com/blackops/ditutorial/services/PrimaryGreetingService.java
@@ -3,6 +3,7 @@ package com.blackops.ditutorial.services;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+//Primary bean to choose on RT
 @Primary
 @Service
 public class PrimaryGreetingService implements GreetingService{
